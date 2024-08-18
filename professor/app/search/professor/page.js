644cfb5c4ce4a311ebe7152288e089d434f43403
Ljf227 <ljfrotton@gmail.com>
2024-08-18 18:59:59 -0400

    made the search pages server components wrapped around the client components

--- professor/app/search/professor/page.js
+++ professor/app/search/professor/page.js
@@ -1,51 +1,16 @@
-"use client"
-import { useSearchParams } from "next/navigation"
-import  ProfessorCard  from "@/components/professor/card";
-import SearchBar from "@/components/searchbar/comp";
-import useSWR from "swr";
-
-const fetcher = async (url) => {
-    const res = await fetch(url);
-    return res.json();
-}
+import SearchInner from "@/components/searchPages/professor";
 
 export default function Search() {
-    const search = useSearchParams();
-
-    const searchQ = search ?  search.get("q") : "";
     
-
-    const { data, error } = useSWR(`/api/search/professor?q=${searchQ}`, fetcher);
 
     
 
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <div className="flex flex-col items-center justify-center flex-grow">
-                <h1 className="text-3xl font-bold mb-8">Search Professors and Courses</h1>
-                <div className="flex space-x-8">
-                <div className="flex flex-col items-center">
-                    <h2 className="text-xl font-semibold mb-4">Professors Search</h2>
-                    <SearchBar type="professor" size = "small" onPage={true} placeholder={searchQ }/>
-                </div>
-                <div className="flex flex-col items-center">
-                    <h2 className="text-xl font-semibold mb-4">Courses Search</h2>
-                    <SearchBar type="course" size ="small" />
-                </div>
-                </div>
-            </div>
 
+            <h1 className="text-3xl font-bold mb-8">Search Professors or Courses</h1>
 
-            <div>
-            {data && data.length > 0 ? (
-                data.map((professor) => (
-                <ProfessorCard key={professor.id} professor={professor} />
-                ))
-            ) : (
-                <p>No professors available.</p> // This message is optional and can be customized or removed
-            )}
-            </div>
-            
+            <SearchInner />
         </main>
     )
 }
