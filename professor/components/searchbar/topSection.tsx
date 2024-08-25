c5b679bd3c30555513a699c21ae642c06ab7a809
Ljf227 <ljfrotton@gmail.com>
2024-08-25 13:57:36 -0400

    another

--- professor/components/searchbar/topSection.tsx
+++ professor/components/searchbar/topSection.tsx
@@ -2,11 +2,11 @@ import SearchBar from '@/components/searchbar/comp';
 const TopSearchSection = () => {
     return(
         <div>
-            <SearchBar type = "course" size = "small" placeholder />
+            <SearchBar type = "course" size = "small" />
             <div className = "py-1" />
-            <SearchBar type ="professor" size = "small" placeholder />
+            <SearchBar type ="professor" size = "small" />
             <div className = "py-1" />
-            <SearchBar type = "school" size = "small" placeholder />
+            <SearchBar type = "school" size = "small" />
       </div>
       );
 }
